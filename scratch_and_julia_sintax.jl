19b4b7c1f616ef1122309aa376659699f2e7cb89
alvarmaciel <alvarmaciel@gmail.com>
2021-06-16 15:54:10 +0000

    Deploying to gh-pages from  @ ed5c32408e233ac043aec521dff91c5cb39c6969 🚀

--- scratch_and_julia_sintax.jl
+++ scratch_and_julia_sintax.jl
@@ -14,34 +14,26 @@ begin
     using PlutoUI
 end
 
-# ╔═╡ 7987cc38-f28d-4ee4-8a1c-738ecc5f81da
-md"""
-!!! hint "Si queres interactuar con esta pagina"
-
-Para usar la notebook hacé click en el botón arriba a la derecha
-
-![](https://alvarmaciel.github.io/paginas-pluto/assets/img/edit.png)
-
-y luego en el botón Binder
-
-![](https://alvarmaciel.github.io/paginas-pluto/assets/img/binder.png)
-
->Toma un rato en cargar todo, pero con estos dos pasos se peude uasr la notebook
-
-"""
+# ╔═╡ ad530bb1-78bb-40a3-93c6-adec42207417
+PlutoUI.TableOfContents(aside=true, title="Contenidos")
 
 # ╔═╡ 74499a68-c58c-11eb-278e-478d05fe686e
 md"""
-# Scratch and Julia syntax 
+# Scratch y sintaxis de Julia
 
-**One aspect of text-based languages that many learners struggle with is understanding the specific syntax (the rules of the language) required. Mistakes in a program are often due to not following these rules, and these mistakes are called syntax errors. Therefore, it is helpful to show the parallels and differences between a language that a learner has already mastered and the one they are trying to learn.**
+**Un aspecto de los lenguajes basados en texto con el muchos estudiantes kidian es la comprención de la sintaxis especícifica (las reglas del lenguaje) requerida. Muchas veces los errores en un programa se deben a que no se siguene estas reglas, y estos erres son denominados errores de sintaxis o _sintax errors_.
+Por lo tanto, es útil mostrar los paralelismos y las diferencias entre un idioma que un alumno ya ha dominado y el que está tratando de aprender.**
 
-In the next two steps I’ll show you a few Scratch blocks and their equivalent code in Python. The list is far from exhaustive, and is intended as a reference guide rather than an exercise to be worked through.
+En los siguientes pasos, les mostraré algunos bloques de Scratch y su código equivalente en Julia. La lista está lejos de ser exhaustiva y está pensada como una guía de referencia más que como un ejercicio sobre el que trabajar.
 
-## Variable assignment
+"""
 
-- In Scratch, a variable needs to be created before it can be assigned a value, whereas in Python, a variable is created when a value is assigned to it."""
 
+# ╔═╡ ee6d8355-542e-4e08-9279-4c05132d7302
+md"""## Asignación de variables
+
+- En scratch una variable necesita ser creada antes de que se le pueda asignar un valor, mientras que en Julia, una variable es creada cuando se le asigna un valor.
+"""
 
 # ╔═╡ 71935899-12a3-4ad4-9084-b8b8f7a3c720
 md"""
@@ -52,13 +44,25 @@ md"""
 ![](https://rpf-futurelearn.s3-eu-west-1.amazonaws.com/block-to-text2/Screenshots/fl-b2t-w1-step06-variableassignment.png)
 """
 
+# ╔═╡ 7987cc38-f28d-4ee4-8a1c-738ecc5f81da
+md"""
+!!! hint "Si querés interactuar con esta página"
+	Para usar la notebook hacé click en el boton arriba a la derecha
+	![](https://alvarmaciel.github.io/paginas-pluto/assets/img/edit.png)
+	
+	y luego en el botón Binder
+	
+	![](https://alvarmaciel.github.io/paginas-pluto/assets/img/binder.png)
+	>Toma un rato en cargar todo, pero con estos dos pasos se peude uasr la notebook
+"""
+
 # ╔═╡ 190473c5-abf5-488c-a81c-63b33d7c91e2
 md"""
 En Julia es necesario rodear los Strings o cualquier texto con comillas smples (') o dobles (")."""
 
 # ╔═╡ c694ee4f-bcb0-4fd5-9fe7-c011523fd864
 md"""
-Corregí la variable `bienvenida` para que sea un string
+Corregí la variable `bienvenida` para que sea un string y poné un mensaje de bienvenida
 """
 
 # ╔═╡ 0bbbe9f5-4870-4d11-886f-84aae2cc2cbd
@@ -74,8 +78,8 @@ end
 
 # ╔═╡ bc4a39ea-74b0-45d1-92d2-de89bb8b1c3d
 md"""
-!!! hint
-    Acordate que los strings o cadenas de textos van entre comillas simples o dobles"
+!!! hint "Ayuda"
+    Los strings o cadenas de textos van entre comillas simples o dobles
 """
 
 # ╔═╡ 4b014a11-c362-42dd-8c3e-7e30cb564fa2
@@ -88,60 +92,84 @@ if (@isdefined bienvenida) && (bienvenida isa String)
 else
 	md"""
 !!! warning "Atencion"
-Todavía falta
+Todavía falta. No cambiaste la variable
 """
 end
 
 # ╔═╡ 0a6df781-6bef-4955-bb8d-75a99fd5f09c
 md"""
-## Increment a variable
+## Incrementar una variable
 
-- In Scratch, a variable’s value can be increased or decreased. To do this, you use a change block with a variable and the number to increment by, using a negative number to decrease the value.
-- To increase or decrease the value of a variable in Julia, you must calculate the new value using the value stored in the variable, and assign this new value to the variable.
+- En Scratch, se puede aumentar o reducir el valor de una variable. Para hacer esto, se una un bloque _cambiar_ con la variable y el número a incrementar, y usamos un número negativo para reducir el valor.
+- Para aumentar o o reducir el valor de una variable en Julia, tienen que calcular el nuevo monto usando el valor guardado en la variable, y asignandole el nuevo vvalor a la variable.
 ``` julia
 puntos = puntos + 1
 ```
-
 """
 
 # ╔═╡ 553e2e22-9b56-42ce-8f5a-4676c9faf024
 md"""
-## Simple output
+## Salida simple output
 
-- In Scratch, you make a sprite talk to provide output to the user of the program.
-- Julia uses print statements to output text, numbers, or symbols.
-- Again in Julia, you need to use single or double quotes if you are printing strings.
+- En Scratch, hacemos que un objeto hable para generar una salida al usuario del programa.
+- Julia usa las sentencias ```print``` y ```println``` para generar una salida de texto, números o simbolos.
+- En Julia, nececitan usar comillas simples o dobles para imprimer texto.
+- En las notebooks interactivas como esta (PlutoJl) la salida de las sentencias ```print``` y ```println``` se visualizan en el REPL. Para visaulizar una salida en PlutoJl con solo enunciar la variable, la celda mostrará su valor.
+
 ![](https://rpf-futurelearn.s3-eu-west-1.amazonaws.com/block-to-text2/Screenshots/fl-b2t-w1-step06-simpleoutput.png)
+
+En julia se escribe:
 ``` julia
 print("Tu puntaje es asombroso!!!")
 print(puntos)
 ```
-
 """
+
+# ╔═╡ fb03bd9c-ed6c-48a5-a3cd-08a4af5b405a
+md"""
+En Pluto simplemente enunciamos las variables, si son más de dos sentencias en un bloque. además podemos incluir texto en formato markdown como este que estás viendo.
+```julia
+begin
+
+end
+```
+"""
+
+# ╔═╡ 1e9e7329-bcf0-4b72-a4c6-d13276d9577f
+begin
+	puntos = 42
+	md""" 
+	En esta celda pueden ver el código. 
+	- Todo lo que está entre ```md``` y las comillas es texto. 
+	- Para incorporar el valor de la variable al texto se usa el símbolo ```$```
+	
+	$bienvenida
+	Tu puntaje es $puntos"""
+end
 
 # ╔═╡ 77c8a723-499e-478d-a519-49b7274ad179
 md"""
-## Conditional loops
+## Estrucutras repetitivas condicionales
 
-- Scratch’s conditional loop repeats until a certain statement is True.
-- Julia’s conditional loop repeats as long as a certain statement is True.
-- Notice that the code that is inside the loop is indented. Indentation is normally four spaces or a single tab. This can be compared to the way the Scratch conditional loop block brackets the code within it.
+- La estructura de repetición condicional en Scratch es repetir **hasta** que cieta condición es Verdadera.
+- La repetición condicional en Julia, repite **mientras**  cierta condición sea Verdadera.
+- Pueden observar que el código dentro de la repetición está identado, La identación no es necesaria en Julia pero se recomienda. Se usan 4 espacios o un tab. Esto puede compararse a la forma en la que la los bloques de repeteción de Scratch encapsulan el código dentro de él.
 ![](https://rpf-futurelearn.s3-eu-west-1.amazonaws.com/block-to-text2/Screenshots/fl-b2t-w1-step06-conditionalloop.png)
 ``` julia
-while puntos > 10
+while puntos < 10
 	print(playing)
 end
-
 ```
-
 """
 
 # ╔═╡ Cell order:
+# ╟─ad530bb1-78bb-40a3-93c6-adec42207417
 # ╟─ab5e05cd-840c-4133-8808-b883c4f3a69e
-# ╟─7987cc38-f28d-4ee4-8a1c-738ecc5f81da
 # ╟─74499a68-c58c-11eb-278e-478d05fe686e
+# ╟─ee6d8355-542e-4e08-9279-4c05132d7302
 # ╟─71935899-12a3-4ad4-9084-b8b8f7a3c720
 # ╟─0af4e606-09c5-4a6c-b4be-9c202058e192
+# ╟─7987cc38-f28d-4ee4-8a1c-738ecc5f81da
 # ╟─190473c5-abf5-488c-a81c-63b33d7c91e2
 # ╟─c694ee4f-bcb0-4fd5-9fe7-c011523fd864
 # ╠═0bbbe9f5-4870-4d11-886f-84aae2cc2cbd
@@ -150,4 +178,6 @@ end
 # ╟─4b014a11-c362-42dd-8c3e-7e30cb564fa2
 # ╟─0a6df781-6bef-4955-bb8d-75a99fd5f09c
 # ╟─553e2e22-9b56-42ce-8f5a-4676c9faf024
+# ╟─fb03bd9c-ed6c-48a5-a3cd-08a4af5b405a
+# ╠═1e9e7329-bcf0-4b72-a4c6-d13276d9577f
 # ╟─77c8a723-499e-478d-a519-49b7274ad179
